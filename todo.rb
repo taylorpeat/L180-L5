88f63ca2553a5a17f6e2e1d11d48faa132a06ef9
Taylor Peat <taylorpeat@hotmail.com>
2017-07-24 23:33:31 -0400

    Preparing for database connection

--- todo.rb
+++ todo.rb
@@ -4,6 +4,7 @@ require "sinatra/content_for"
 require "tilt/erubis"
 require "rb-readline"
 require "pry"
+require_relative "session_persistence"
 
 configure do
   enable :sessions
@@ -11,68 +12,7 @@ configure do
   set :erb, :escape_html => true
 end
 
-class SessionPersistence
-  
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
 
-  def find_list(id)
-    @session[:lists].find{ |list| list[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(all_lists)
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def delete_list(id)
-    @session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def update_list(list_name, id)
-    list = find_list(id)
-    list[:name] = list_name
-  end
-
-  def add_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    todo_id = next_element_id(list[:todos])
-    list[:todos] << { id: todo_id, name: todo_name, completed: false }
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo(list_id, todo_id, is_completed)
-    list = find_list(list_id)
-    todo = list[:todos].find { |todo| todo[:id] == todo_id }
-    todo[:completed] = is_completed
-  end
-
-  def mark_all_complete(list_id)
-    list = find_list(list_id)
-    
-    list[:todos].each do |todo|
-      todo[:completed] = true
-    end
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |todo| todo[:id] }.max || 0
-    max + 1
-  end
-
-end
 
 helpers do
   def list_complete?(list)
